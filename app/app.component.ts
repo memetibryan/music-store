b09b0958d2d43c16ba7b46f16b78b4ef7a710d9f
memetibryan <memetibryan@gmail.com>
2017-06-01 16:51:09 +0300

    first typescript code,that is,app.components.ts created

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '',
+  selector: 'my-app',
   template: `
-
+  <div class="container">
+  <h3>Music List:</h3>
+  <p>Below are some of the favourite music we have in store:</p>
+  <h3>{{musicGenre}}</h3>
+  <div class="description" *ngFor="let description of descriptions">
+  <ol>
+  <li>Song name:- {{musicDescription.name}}</li>
+  <li>Artist:- {{musicDescription.artist}}</li>
+  <li>Year Produced:- {{musicDescription.year}}</li>
+  </ol></div>
+  </div>
   `
 })
 
 export class AppComponent {
+  musicGenre: string = 'Riddims';
+  musicDescription: Description = new Description("Turn Me On","Konshens", 2017);
+  descriptions: Description[] = [
+    new Description("Puppy Tail", "Demarco", 2016),
+    new Description("Dont", "The Stooges", 1970),
+    new Description("Twilight of the Thunder God", "Amon Amarth", 2008),
+    new Description("Dilate", "Ani DiFranco", 1996),
+    new Description("Chopin - Complete Nocturnes", "Brigitte Engerer", 2015),
+    new Description("Axis Bold As Love", "The Jimi Hendrix Experience", 1967)
+  ];
+}
 
+export class Description {
+  constructor (public name: string, public artist: string, public year: number) {  }
 }
